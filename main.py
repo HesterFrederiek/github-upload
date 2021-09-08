406263e46a164ba7b99cec781f71ef1f8f360087
hesterfrederiek <hesterpieters@gmail.com>
2021-09-08 16:00:31 +0200

    add text to print

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Hester')
-    print("How are you?")
+    print("How are you doing?")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
